5d56a8bc8af9a818b6a44264145fe58ede2d2360
class PythonAddict <classpythonaddike@gmail.com>
2021-03-04 18:19:58 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BFInterpreter
+{
+	class Program
+	{
+		public static int pointer = 0;
+		public static List<int> memory_blocks = new List<int>();
+		public static string BFCode;
+
+		static void Main(string[] args)
+		{
+			foreach (string i in args)
+			{
+				using (FileStream fs = File.Open(i, FileMode.Open))
+				{
+					BFCode = "";
+
+					byte[] b = new byte[1024];
+					UTF8Encoding decoder = new UTF8Encoding(true);
+
+					while (fs.Read(b, 0, b.Length) > 0)
+					{
+						BFCode += decoder.GetString(b);
+					}
+				}
+
+				memory_blocks.Add(0);
+
+				try
+				{
+					ParseBF(BFCode);
+				}
+				catch
+				{
+					Console.WriteLine("\nError!");
+					break;
+				}
+			}
+			Console.Write("\nPress any key to exit . . . .");
+			Console.ReadKey();
+		}
+
+		static void ParseBF(string code)
+		{
+			int CurrentChar = 0, NumLoops, CharCopy;
+			char chr;
+
+			while (CurrentChar != code.Length)
+			{
+				if (code[CurrentChar] == '+')
+				{
+					memory_blocks[pointer] += 1;
+				}
+				else if (code[CurrentChar] == '-')
+				{
+					memory_blocks[pointer] -= 1;
+				}
+				else if (code[CurrentChar] == '>')
+				{
+					pointer += 1;
+				}
+				else if (code[CurrentChar] == '<')
+				{
+					pointer -= 1;
+				}
+				else if (code[CurrentChar] == '.')
+				{
+					Console.Write((char)memory_blocks[pointer]);
+				}
+				else if (code[CurrentChar] == ',')
+				{
+					chr = Console.ReadKey().ToString().ToCharArray()[0];
+					memory_blocks[pointer] = (int)chr;
+				}
+				else if (code[CurrentChar] == '[')
+				{
+					NumLoops = 1;
+					CharCopy = CurrentChar + 1;
+
+					while (NumLoops != 0)
+					{
+						CurrentChar += 1;
+						if (code[CurrentChar] == '[')
+						{
+							NumLoops += 1;
+						}
+						else if (code[CurrentChar] == ']')
+						{
+							NumLoops -= 1;
+						}
+					}
+
+					while (memory_blocks[pointer] != 0)
+					{
+						ParseBF(code.Substring(CharCopy, CurrentChar - CharCopy));
+					}
+
+
+				}
+
+				CurrentChar += 1;
+				while (memory_blocks.Count <= pointer)
+				{
+					memory_blocks.Add(0);
+				}
+			}
+		}
+	}
+}
---
+++ ReadMe.md
@@ -0,0 +1,13 @@
+﻿# A BrainF Interpreter Written in C#
+
+## Setup
+
+1. To use this project, first clone this repo onto your local machine:
+```sh
+git clone https://github.com/classPythonAddike/BFInterpreter.git
+```
+2. If you are on a 64-bit Windows computer, you just got lucky! I've already compiled this program, and it can be found in the [/bin/Debug](Debug folder)!
+3. However, if you are not on Windows, or not on a 32-bit computer, you'll need to build this project again. You can do this from the terminal, or with an IDE like Mono, Visual Studio Code, Rider, or Visual Studio ~~or Notepad++~~
+4. Run your BrainF code with `> BFInterpreter.exe file.bf` where `file.bf` is the file you want to run!
+
+A big thank you to ~~@classPythonAddike~~ for making this repo possible!
